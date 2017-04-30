a779db7312052a15b4188f2e2c0eb27e7f3fb9af
sindrgro94 <sindre.grostad@icloud.com>
2017-04-30 11:29:43 +0200

    småtterier

--- c.m
+++ c.m
@@ -1,6 +1,5 @@
 function [c_x,c_y] = c(j,THETA,L, P)
-    c1 = @(theta,L,p) [sum(L.*cos(cumsum(theta)));sum(L.*sin(cumsum(theta)))]-p;
-    
+    c1 = @(theta,l,p) [sum(l.*cos(cumsum(theta)));sum(l.*sin(cumsum(theta)))]-p;
     C = c1(THETA(:,j),L,P(:,j));
     c_x = C(1);
     c_y = C(2);
