953272089ffda5096e8eaa973ba8259a28913452
Halvorsg <halvor.snersrud@gmail.com>
2017-04-30 11:23:14 +0200

    mer quadratic

--- quadratic_penalty.m
+++ quadratic_penalty.m
@@ -1,11 +1,19 @@
 function quadratic_penalty(THETA,L,P,tol,max_iter)
+tol = 0.1;
 my = 1;
 k = 0;
 while norm(dQ(THETA,L,P,my)) > 0.01 && max_iter > k
-    THETA = gradient_descent_Q(THETA,L,P,my,0.1);
-    my = my*1.5;
+    [THETA,n,flag] = gradient_descent_Q(THETA,L,P,my,tol);
+    if n < 10 && flag == false
+        my = my*1.5;
+        tol = 0.9*tol;
+    elseif flag == true || n >= 10
+        my = my*1.5;
+        tol = tol*0.9;
+        
+    end
     k = k+1;
-    dd = norm(dQ(THETA,L,P,my))
+    norm_dd = norm(dQ(THETA,L,P,my))
 end
 
 end
