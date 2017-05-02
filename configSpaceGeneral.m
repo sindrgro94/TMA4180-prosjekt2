81ef96c82cc537565e7faea7ec8066a9a75a3c97
sindrgro94 <sindre.grostad@icloud.com>
2017-05-02 11:23:19 +0200

    config space ikke rekursiv

--- configSpaceGeneral.m
+++ configSpaceGeneral.m
@@ -1,7 +1,8 @@
 function configSpaceGeneral(L,c)
 % This function plots the set of all reachable points for a robot with
 % segments lengths L = [l1,l2,....,ln] and with angles -c to c.
-angles=linspace(-c,c,30);
+% For robot arms with 4 arms the function uses approximately 1 minute.
+angles=linspace(-c,c,50);
 n=length(angles);
 arms = length(L);
 points=zeros(2,n^arms);
@@ -9,31 +10,38 @@ theta = ones(1,arms);
 theta=theta*(-c);
 dtheta=angles(2)-angles(1);
 count=0;
-joint = 1;
+joint = arms;
 tic
-[points,~,~] = getConfigSpace(L,dtheta,theta,count,points,n,joint,arms,c);
-toc
-plot(points(1,:),points(2,:),'b*')
-hold on
-plot(0,0,'r*')
-axis([min(points(1,:))-1,max(points(1,:))+1,min(points(2,:))-1,max(points(2,:))+1])
-title('The set of all reachable points')
-end
-
-function [points,count,theta] = getConfigSpace(L,dtheta,theta,count,points,n,joint,arms,c)
-    for i=1:n
-        if joint<arms
-            [points,count,theta] = getConfigSpace(L,dtheta,theta,count,points,n,joint+1,arms,c);
-        end
-        if joint == arms
+notFinished = true;
+while notFinished
+    if joint==arms
+        for i=1:n
             THETA = cumsum(theta);
             x = dot(L,cos(THETA));
             y = dot(L,sin(THETA));
             count = count+1;
             points(1,count)=x;
             points(2,count)=y;
+            theta(arms) = theta(arms)+dtheta;
         end
-        theta(joint) = theta(joint)+dtheta;
+        theta(arms) = -c;
+        joint = joint-1;
     end
-    theta(joint) = -c;
+    theta(joint) = theta(joint)+dtheta;
+    if theta(joint)>c
+        if joint == 1
+            notFinished = false;
+        end
+        theta(joint) = -c;
+        joint = joint-1;
+    else
+        joint = joint + 1;
+    end
+end
+toc
+plot(points(1,:),points(2,:),'b*')
+hold on
+plot(0,0,'r*')
+axis([min(points(1,:))-1,max(points(1,:))+1,min(points(2,:))-1,max(points(2,:))+1])
+title('The set of all reachable points')
 end
---
+++ trash/configspacerec.m
@@ -0,0 +1,39 @@
+function configspacerec(L,c)
+% This function plots the set of all reachable points for a robot with
+% segments lengths L = [l1,l2,....,ln] and with angles -c to c.
+angles=linspace(-c,c,50);
+n=length(angles);
+arms = length(L);
+points=zeros(2,n^arms);
+theta = ones(1,arms);
+theta=theta*(-c);
+dtheta=angles(2)-angles(1);
+count=0;
+joint = arms;
+tic
+[points,~,~] = getConfigSpace(L,dtheta,theta,count,points,n,joint,arms,c);
+toc
+plot(points(1,:),points(2,:),'b*')
+hold on
+plot(0,0,'r*')
+axis([min(points(1,:))-1,max(points(1,:))+1,min(points(2,:))-1,max(points(2,:))+1])
+title('The set of all reachable points')
+end
+
+function [points,count,theta] = getConfigSpace(L,dtheta,theta,count,points,n,joint,arms,c)
+    for i=1:n
+        if joint<arms
+            [points,count,theta] = getConfigSpace(L,dtheta,theta,count,points,n,joint+1,arms,c);
+        end
+        if joint == arms
+            THETA = cumsum(theta);
+            x = dot(L,cos(THETA));
+            y = dot(L,sin(THETA));
+            count = count+1;
+            points(1,count)=x;
+            points(2,count)=y;
+        end
+        theta(joint) = theta(joint)+dtheta;
+    end
+    theta(joint) = -c;
+end
