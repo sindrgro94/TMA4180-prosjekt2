8891c7385d7d194a9d3ba0e4bc315373c45e515f
Halvorsg <halvor.snersrud@gmail.com>
2017-05-02 12:06:07 +0200

    constr

--- Augmentet_Lagrangian.m
+++ Augmentet_Lagrangian.m
@@ -4,32 +4,20 @@ function svar = Augmentet_Lagrangian(THETA,L,P,max_iter)
 if is_Outside(L,P) == true
     fprintf('At least one of the points are outside, no solution exist.\n');
     return
-else
+end
 
 %%Initialiserer 
 TOL = 0.01;
 my = 1;
 [~,s] = size(THETA);
 lambdas = ones(s,2);
-tol = 0;
-for j = 1:s
-    [c_x,c_y] = c(j,THETA,L, P);
-    tol = tol +abs(c_x)+abs(c_y);
-end
+[~,tol] = update_lambdas_tol(THETA,lambdas,L,P,my);
 k = 0;
 %%Augmentet Lagrangian
 while norm(dLag(THETA,lambdas,L,P,my)) > TOL && max_iter > k
     k = k+1;
-    %[THETA,rounds] = gradient_descent(THETA,lambdas,L,P,my, tol);
     [THETA] = quasi_Newton(THETA,lambdas,L,P,my, tol);
-    %tol_old = tol;
-    tol = 0;
-    for j = 1:s
-        [c_x,c_y] = c(j,THETA,L, P);
-        lambdas(j,1) = lambdas(j,1)-my*c_x;
-        lambdas(j,2) = lambdas(j,2)-my*c_y;
-        tol = (tol +abs(c_x)+abs(c_y));
-    end
+    [lambdas,tol] = update_lambdas_tol(THETA,lambdas,L,P,my);
     my = my*2;
     dd = norm(dLag(THETA,lambdas,L,P,my))
     
@@ -37,3 +25,18 @@ end
 svar = THETA;
 plotHandMovement(THETA,L,P)
 end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [lambdas,tol] = update_lambdas_tol(THETA,lambdas,L,P,my)
+    [~,s] = size(THETA);
+    tol = 0;
+        for j = 1:s
+            [c_x,c_y] = c(j,THETA,L, P);
+            lambdas(j,1) = lambdas(j,1)-my*c_x;
+            lambdas(j,2) = lambdas(j,2)-my*c_y;
+            tol = (tol +abs(c_x)+abs(c_y));
+        end
+end
+
+
+
