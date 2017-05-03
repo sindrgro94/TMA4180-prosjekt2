939cc47030fb00256b287157bf1fc9a613f85a34
Halvorsg <halvor.snersrud@gmail.com>
2017-05-03 09:01:59 +0200

    Mer riktig constr_gradC

--- constr_gradC.m
+++ constr_gradC.m
@@ -1,9 +1,18 @@
-function [grad_u,grad_l] = constr_gradC(THETA)
+function [grad_u,grad_l] = constr_gradC(THETA,angle)
+upper = angle;
+lower = -angle;
     [n,s] = size(THETA);
     grad_u = zeros(n*s,1);
     grad_l = zeros(n*s,1);
     for i = 1:n*s
-        grad_u(i) = -1;
-        grad_l(i) = 1;
+        %% nytt
+        if THETA(i) > upper
+            grad_u(i) = 1;
+        elseif THETA(i) < lower
+            grad_l(i) = -1;
+        end
+        %%
+%         grad_u(i) = -1;
+%         grad_l(i) = 1;
     end
 end
