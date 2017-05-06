07e98410e82eb9799f7fa81eda58746ff40eb67a
sindrgro94 <sindre.grostad@icloud.com>
2017-05-06 13:24:58 +0200

    til innlevering

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # TMA4180-prosjekt2
+The main functions are:
+- Augmentet_Lagrangian, solving the first problem without inequality constraints.
+- BC_Augmented_Lagrangian, solving the second problem with inequality constraints.
+- configSpaceGeneral, wich draws the configuration space for an arm with:
+    - L = vector with arm lengths
+    - c = angular constraint
+    - p = 2xs matrix with x and y values for points.
+    - plotOrNot as true if you want to make a plot
+    - countdown as true if you want to get a countdown each 5 percent.
+This function is not recommended to run with more than 4 arms because of the exponential grow.
+
+
+demonstration.m is a script where each function can be ran seperately. More comments are in that script.
--- demonstration.m
+++ demonstration.m
@@ -25,11 +25,12 @@ plotHandMovement(answer,L,P);
 
 %% Task2 Augmented lagrangian for both equality and inequality
 % To make the method terminate and don't use too long time, the tolerance for the 2-norm of the gradient
-% to the lagrangian is set to 0.1 and stepSelection is false meaning we use
+% to the lagrangian is set to 0.1 and stepSelection = false meaning we use
 % only backtracking. When we do this we can not guarantee H to be pos.def so we reset H to be the identity matrix 
-% at each step. Then the method is technically gardient descent.
+% at each step. Then the method is technically gradient descent.
 % If stepSelection is true we use find_alpha_constrained.m to find stepsizes,
-% wich choose stepsizes based on wolfe conditions. However in some cases
+% wich choose stepsizes based on wolfe conditions. However in some cases eh
+% have experienced that
 % it will not manage to  find a stepsize to make both wolfe-conditions satisfied.
 % Then the H-matrix is reset to the identity matrix. In this way it is a
 % BFGS method if everething works out, but in some cases it is just a
@@ -44,7 +45,7 @@ stepSelection = true;
 L = [3;2;2];
 P = [5,4,6,4,5;0,2,0.5,-2,-1];
 % P = [9,4,6,4,5;0,2,0.5,-2,-1]; %this will terminate early because of the first point (9,0)
-% P = [-1,-3,-3,0,5;5,3,-4,5,2];
+P = [-1,-3,-3,0,5;5,3,-4,5,2];
 THETA = ones(length(L),length(P));
 [~,points] = size(P);
 for point = 1:points
